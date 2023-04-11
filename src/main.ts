7d6f91022237c90be0ab686df02fcb4c60c04015
JosepDev23 <josepdev23@gmail.com>
2023-04-11 20:39:12 +0200

    change api direction

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(3001);
 }
