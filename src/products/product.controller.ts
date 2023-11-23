f8b9a3d2f2452b042d2358197b631bf33974b40a
JosepDev23 <josepdev23@gmail.com>
2023-11-24 00:17:23 +0100

    feature: filter by supermarket

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,41 +1,76 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
-import { ProductService } from './product.service';
-import Product from './product.schema';
-import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { DeleteResultDto } from 'src/delete-result-dto';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common'
+import { ProductService } from './product.service'
+import Product from './product.schema'
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { DeleteResultDto } from 'src/delete-result-dto'
 
 @ApiTags('products')
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) { }
+  constructor(private readonly productService: ProductService) {}
 
   @Get()
   @ApiOperation({ summary: 'Get all products' })
   @ApiResponse({ status: 200, description: 'Products list', type: [Product] })
-  @ApiQuery({ name: 'name', required: false, type: String, description: 'Product name for filtering' })
-  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Products limit by page' })
-  @ApiQuery({ name: 'offset', required: false, type: Number, description: 'Initial index for pagination' })
-  @ApiQuery({ name: 'order', required: false, type: String, description: 'Price order (asc or desc)' })
+  @ApiQuery({
+    name: 'supermarket',
+    required: false,
+    type: String,
+    description: 'Supermarket name for filtering',
+  })
+  @ApiQuery({
+    name: 'name',
+    required: false,
+    type: String,
+    description: 'Product name for filtering',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Products limit by page',
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    type: Number,
+    description: 'Initial index for pagination',
+  })
+  @ApiQuery({
+    name: 'order',
+    required: false,
+    type: String,
+    description: 'Price order (asc or desc)',
+  })
   async getProducts(
+    @Query('supermarket') supermarket?: string,
     @Query('name') name?: string,
     @Query('limit') limit?: number,
     @Query('offset') offset?: number,
-    @Query('order') order?: string
+    @Query('order') order?: string,
   ): Promise<Product[]> {
-    return this.productService.findAll(name, limit, offset, order);
+    return this.productService.findAll(supermarket, name, limit, offset, order)
   }
 
   @Post()
   @ApiOperation({ summary: 'Create a new product' })
-  @ApiResponse({ status: 201, description: 'Product created successfully', type: Product })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: Product,
+  })
   async postProduct(@Body() product: Product): Promise<Product> {
-    return this.productService.save(product);
+    return this.productService.save(product)
   }
 
   @Delete()
   @ApiOperation({ summary: 'Deletes all products' })
-  @ApiResponse({ status: 200, description: 'Number of deleted products', type: DeleteResultDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Number of deleted products',
+    type: DeleteResultDto,
+  })
   async deleteAllProducts(): Promise<{ deletedCount?: number }> {
-    return this.productService.deleteAll();
+    return this.productService.deleteAll()
   }
 }
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,27 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
 
-import { Model } from 'mongoose';
+import { Model } from 'mongoose'
 
-import Product from './product.schema';
+import Product from './product.schema'
 
 @Injectable()
 export class ProductService {
-  constructor(@InjectModel('Product') private productModel: Model<Product>) { }
+  constructor(@InjectModel('Product') private productModel: Model<Product>) {}
 
   async save(product: Product): Promise<Product> {
-    const savedProduct = new this.productModel(product);
-    return savedProduct.save();
+    const savedProduct = new this.productModel(product)
+    return savedProduct.save()
   }
 
-  async findAll(name: string, limit: number, offset: number, priceOrder: string): Promise<Product[]> {
-    const filter = { name: new RegExp(name, 'i') };
-    const sortPriceOrder = priceOrder === 'desc' ? -1 : 1;
-    return this.productModel.find(filter).sort({ price: sortPriceOrder }).limit(limit).skip(offset).exec();
+  async findAll(
+    supermarket: string,
+    name: string,
+    limit: number,
+    offset: number,
+    priceOrder: string,
+  ): Promise<Product[]> {
+    const filter = {
+      name: new RegExp(name, 'i'),
+      supermarket: new RegExp(supermarket, 'i'),
+    }
+    const sortPriceOrder = priceOrder === 'desc' ? -1 : 1
+    return this.productModel
+      .find(filter)
+      .sort({ price: sortPriceOrder })
+      .limit(limit)
+      .skip(offset)
+      .exec()
   }
 
   async deleteAll(): Promise<{ deletedCount?: number }> {
-    return this.productModel.deleteMany().exec();
+    return this.productModel.deleteMany().exec()
   }
-
 }
