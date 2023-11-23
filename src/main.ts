62de3f1fbc178b6bf30483a4a2cfa9111208f5ec
JosepDev23 <josepdev23@gmail.com>
2023-11-24 00:12:52 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule)
+
   const config = new DocumentBuilder()
     .setTitle('Products service')
     .setDescription('Service to do CRUD operations with the Product entity')
     .setVersion('0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('', app, document)
 
-  await app.listen(3001);
+  app.enableCors()
+  await app.listen(3001)
 }
 
-bootstrap();
+bootstrap()
